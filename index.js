f2625528b578dbb1a92705b98866dca3bb787280
akie000099 <akiesmh@gmail.com>
2021-04-16 18:27:30 +0900

    4/16更新分、charset作れない問題

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const config = {
 	head:[
-			{element: "title", contents:"CAFE Sweet Time"},
-			{element:"meta", contents:"", options:{name:"viewport", content:"width=device-width,initial-scale=1"}},
-			{element:"meta", contents:"", options:{charset:"UTF-8"}}]
+			{element: "title", contents:"CAFE SWEETTIME"},
+			{element:"meta", contents:"", options:{charset:"UTF-8"}},
+			{element:"meta", contents:"", options:{name:"viewport", content:"width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"}},
+
+			{element:"link", contents:"", options:{rel:"icon", href:"https://akiemunakata.net/cafesweettime/favicon.ico"}},
+			{element:"link", contents:"", options:{rel:"apple-touch-icon", href:"https://akiemunakata.net/cafesweettime/apple-touch-icon.png"}},
+			{element:"link", contents:"", options:{rel:"stylesheet", href:"https://akiemunakata.net/cafesweettime/jquery.bxslider.css"}},
+			{element:"link", contents:"", options:{rel:"apple-touch-icon", href:"https://akiemunakata.net/cafesweettime/apple-touch-icon.png"}},
+			{element:"link", contents:"", options:{rel:"apple-touch-icon", href:"https://akiemunakata.net/cafesweettime/meanmenu.css"}},
+			{element:"script", contents:"", options:{type:"text/javascript", charset:"UTF-8", src:"https://maps.googleapis.com/maps-api-v3/api/js/44/10/intl/ja_ALL/common.js"}},
+		//{element:"script", contents:"", options:{src:css.js}}
+	],
+	body:[
+		{element: "header", contents:""},
+		{element: "nav", contents:"", options:{id:"nav"}},
+		{element:"main", contents:""},
+		{element:"footer", contents:""}
+	],
+	menus: ["健康","運動","肩こり","検索"],
 }
 //引数には親、タグ、テキスト、オプション（属性など）を入れます。
 const createChildTag = (parent, tag, text, options) => {
@@ -17,6 +33,25 @@ const createChildTag = (parent, tag, text, options) => {
 	// タグを返します。
 	return element;
 }
+//ul/liを実装したい。
+//ul自体にタグを実装するには？
+//それともrenderloopを3階層分作ったほうが効率的なのか
+const createListTag = (parent,  tag, text, olul, options) => {
+	(olul === ul) ? document.createElement('ul') : document.createElement('ol');
+
+	//その後タグ(element)をつくります。
+	if(tag)element = document.createElement(tag);
+	element.innerText = text;
+	//もしオプションがあった場合のメソッドを作ります。
+	//elementと同じ場所に入れるため、mapメソッドを使用し、keyを代入し入れるようにします。
+	if(options) Object.keys(options).map(key => element[key] = options[key]);
+	//親の中にelementをいれるメソッドを作ります。
+	if(parent)parent.appendChild(element);
+	// タグを返します。
+	return element;
+}
+
+
 //配列のみを繰り返す場合
 const renderLoopArrayParts = (parent, children) => {
 	children.map(child=>createChildTag(parent,child.children.element,child.children.contents,child.children?.options))
@@ -36,6 +71,11 @@ const renderLoopArrayParts = (parent, children) => {
 window.addEventListener('DOMContentLoaded', () => {
 	config.head.map(child => createChildTag(document.head, child.element, child.contents, child?.options));
 //4/14、ここまでひらめくのに20分。でも実は1時間は悩んだ。やった！進んだ。
+	config.body.map(child => createChildTag(document.body, child.element, child.contents, child?.options));
+
+	const nav = document.getElementById('nav');
+	config.menus.map((menuItem,index) => (config.menus.length - 1 === index) ? createChildTag(nav,'a',menuItem, {style:"float: right"}) : createChildTag(nav,'a',menuItem));
+//4/16ここまでひらめくのに30分。ulとliをうまい具合実装したい。
 
 });
 
